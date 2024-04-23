f3a6daab83e088e36047cbc845955432bce139a7
Koki Nanao <minsokosoko0107@gmail.com>
2024-04-23 18:33:41 +0900

    feat:追加
    タスクの取得と作成の追加

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,35 @@ import type { Express, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const app: Express = express();
+app.use(express.json());
 const PORT: number = 8080;
 
 const prisma = new PrismaClient();
 
 app.get('/', async (req: Request, res: Response) => {
-  const allTodo = await prisma.todo.findMany();
-  return res.json(allTodo);
+  try {
+    const allTodo = await prisma.todo.findMany();
+    return res.json(allTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.post('/create', async (req: Request, res: Response) => {
+  try {
+    const { title, isCompleted } = req.body;
+    const createTodo = await prisma.todo.create({
+      data: {
+        title,
+        isCompleted,
+      },
+    });
+    return res.json(createTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 app.listen(PORT, () => {
